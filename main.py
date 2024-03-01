3eea99fdb5a4368bfa1fa492817962eb61f5d891
Ben Sherman <bcanfieldsherman@gmail.com>
2024-03-01 12:12:00 -0800

    add test yaml into wandb config

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import wandb
+import yaml
 from wandb.sdk.launch import template
 
 config = {
     "param1": 1,
     "param2": 2,
-    "param3": 3,
-    "param4": [1, 2, 3],
 }
 
 run = wandb.init(config=config)
 
 template.add_parameter(template.WandbConfigKeys(include=["param1", "param2"]))
 template.add_parameter(template.ConfigFile("test.yaml"))
+
+wandb.config.update(yaml.load(open("test.yaml"), Loader=yaml.FullLoader))
 
 run.finish()
